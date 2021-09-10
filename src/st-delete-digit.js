7cd05df9ccc9ad3a764ea530accb8ff41fa318c4
YakovLya <64806983+YakovLya@users.noreply.github.com>
2021-09-10 22:17:51 +0300

    feat: new solutions

--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -11,7 +11,17 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function deleteDigit(n) {
+  let ans;
+  let pos = -1;
+  let a = n.toString();
+  for(let i = 0; i < a.length - 1; i ++)
+    if (a.charCodeAt(i) < a.charCodeAt(i + 1) && pos == -1)
+      pos = i;
+  if (pos == -1)
+    a = a.slice(0, a.length - 1);
+  else 
+    a = a.slice(0, pos) + a.slice(pos + 1, a.length);
+  ans = Number(a);
+  return ans;
 }
--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,26 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  if (str.length < 1)
+    return "";
+  let ans = "";
+  let k = 1;
+  let c = str[0];
+  for(let i = 1; i < str.length; i ++)
+    if (str[i] == c)
+      k ++;
+    else{
+      if (k > 1)
+        ans = ans + k.toString() + c;
+      else
+        ans += c;
+      c = str[i];
+      k = 1;
+    }
+  if (k > 1)
+    ans = ans + k.toString() + c;
+  else
+    ans += c;
+  return ans;
 }
