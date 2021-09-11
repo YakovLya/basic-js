956ea14a439417f11326c32aa15e968344104122
YakovLya <64806983+YakovLya@users.noreply.github.com>
2021-09-11 13:25:32 +0300

    feat: new solutions

--- src/st-common-character-count.js
+++ src/st-common-character-count.js
@@ -11,7 +11,17 @@ import { NotImplementedError } from '../extensions/index.js';
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-export default function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getCommonCharacterCount(s1, s2) {
+  let ans = 0;
+  let a = new Array(26);
+  let b = new Array(26);
+  for(let i = 0; i < 26; i ++)
+    a[i] = b[i] = 0;
+  for(let i = 0; i < s1.length; i ++)
+    a[s1.charCodeAt(i) - 'a'.charCodeAt(0)] ++;
+  for(let i = 0; i < s2.length; i ++)
+    b[s2.charCodeAt(i) - 'a'.charCodeAt(0)] ++;
+  for(let i = 0; i < 26; i ++)
+    ans += Math.min(a[i], b[i]);
+  return ans;
 }
--- src/st-sort-by-height.js
+++ src/st-sort-by-height.js
@@ -11,7 +11,21 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-export default function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function sortByHeight(arr) {
+  let a = [];
+  for(let i = 0; i < arr.length; i ++)
+    if (arr[i] != -1)
+      a.push(arr[i]);
+  a.sort(function(a, b){
+    return a - b;
+  });
+  let j = 0;
+  for(let i = 0; i < arr.length; i ++)
+    if (arr[i] != -1){
+      arr[i] = a[j];
+      j ++;
+    }
+  return arr;
 }
+
+console.log(sortByHeight( [ 11, 16, 2, 2, 4, 9 ]));
