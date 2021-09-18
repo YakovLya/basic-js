2b1c547e2a4cc8b0ad1d2dd0bd8d0490c8149938
YakovLya <64806983+YakovLya@users.noreply.github.com>
2021-09-18 22:01:21 +0300

    feat: vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,71 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(...args){
+    if(args.length !== 0 && args[0] === false)
+      this.isReverse = 1;
+    else
+      this.isReverse = 0;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(str, keyBad) {
+    if (!str || !keyBad)
+      throw Error('Incorrect arguments!');
+    let key = "";
+    while(key.length < str.length)
+      key += keyBad;
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+    let j = -1;
+    for(let i = 0; i < str.length; i ++){
+      j ++;
+      let sum = str.charCodeAt(i) - 65;
+      if (sum < 0 || sum > 25){
+        j --;
+        continue;
+      }
+      sum += key.charCodeAt(j) - 65;
+      sum = sum % 26;
+      str = str.slice(0, i) + String.fromCharCode(sum + 65) + str.slice(i + 1);
+    }
+    if (this.isReverse){
+      let splitStr = str.split("");
+      let reverseArr = splitStr.reverse();
+      str = reverseArr.join("");
+    }
+    return str;
+  }
+  decrypt(str, keyBad) {
+    if (!str || !keyBad) 
+      throw Error('Incorrect arguments!');
+    let str = args[0];
+    
+    let keyBad = args[1];
+    let key = "";
+    while(key.length < str.length)
+      key += keyBad;
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+    let j = -1;
+    for(let i = 0; i < str.length; i ++){
+      j ++;
+      let sum = str.charCodeAt(i) - 65;
+      if (sum < 0 || sum > 25){
+        j --;
+        continue;
+      }
+      sum -= key.charCodeAt(j) - 65;
+      if (sum < 0)
+        sum = sum + 26;
+      str = str.slice(0, i) + String.fromCharCode(sum + 65) + str.slice(i + 1);
+    }
+    if (this.isReverse){
+      let splitStr = str.split("");
+      let reverseArr = splitStr.reverse();
+      str = reverseArr.join("");
+    }
+    return str;
   }
 }
+
